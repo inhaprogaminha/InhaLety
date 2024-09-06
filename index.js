f9c959e25b1d4d52f5e709a47e974604503a5e26
Yasmin Vitória <163542702+inhaprogaminha@users.noreply.github.com>
2024-09-06 11:36:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,10 @@ const locPerfil = document.querySelector('.loc-perfil')
 const getInfos = async () =>{
     const response = await fetch('https://api.github.com/users/inhaprogaminha')
     const data = await response.json()
-    console.log(data)
-    if(!data.name){
-        titulo.innerHTML = 'Yasmin'
-        reposPerfil.innerHTML = '60'
-        imagemPerfil.setAttribute('src', 'data.https://placeholder.com/100x100')
-        bioPerfil.document = 'Sou Yasmin Desen..'
-        segPerfil.innerHTML= 'Seguidores 123 e seguindo 123 perfil'
-        locPerfil.innerHTML = 'BAYEUX'
-
-
-
-    }else{
-        titulo.innerHTML = data.name
-        reposPerfil.innerHTML = data.public_reposimagemPerfil.setAttribute('src',data.avatar_url)
-        bioPerfil.document = data.bio
-        segPerfil.innerHTML= `seguidores ${data.followers} e seguindo ${data.following} perfil`
-        locPerfil.innerHTML = data.location
-
-
-    }
-
-    
+    titulo.innerHTML = data.name
+    reposPerfil.innerHTML = data.public_reposimagemPerfil.setAttribute('src',data.avatar_url)
+    bioPerfil.document = data.bio
+    segPerfil.innerHTML= `seguidores ${data.followers} e seguindo ${data.following} perfil`
+    locPerfil.innerHTML = data.location    
 } 
 getInfos()
